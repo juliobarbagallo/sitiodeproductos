20245cf2f76cf64499c49a2e041110ec431973e3
Ezequiel Yacar <ezequielyacar@Air-de-Ezequiel.fibertel.com.ar>
2020-11-19 17:23:42 -0300

    Eze commit

--- index.js
+++ index.js
@@ -14,18 +14,73 @@ nunjucks.configure('views', {
     express: app
 });
 
+session = require('express-session');
+
+app.use(session({
+  secret: 'elSecreto',
+  name: 'sessionId',
+  proxy: true,
+  resave: true,
+  saveUninitialized: true ,
+  cookie: { maxAge: 24 * 60 * 60 * 1000  }  
+}));
+
+var Allusers = function(req, res, next) {
+    if (req.session.logeado === 'ok')
+      return next();
+    else
+      return res.status(401).send(`Tu sesion expiró o No estas logeado. <a href="/">Home</a>`);
+  };
+
 app.get('/', function(req, res) {    
     fetch('https://raw.githubusercontent.com/juliobarbagallo/sitiodeproductos/master/data/productos.json')
     .then(response => response.json())
     .then(productos => res.render('index.html', {productos:productos}))
 });
 
-app.get('/producto/:id', function(req, res) {
-    fetch('https://raw.githubusercontent.com/juliobarbagallo/sitiodeproductos/master/data/productos.json')
-    .then(response => response.json())
-    .then(productos => res.render('producto.html', {productos:productos, id:req.params.id}))
+app.get('/carrito', Allusers, function(req, res) {    
+res.send('hola')    
 });
 
+app.get('/login', function(req, res) {
+var ref = ''
+var dato = ''
+/* 
+var datos = [{"user":"dami","password":"1234"},{"user":"coco","password":"1111"}]
+for (dato of datos){
+if ( req.body.user === dato.user && req.body.pass === dato.password){
+req.session.logeado === 'ok'}
+ref = '/' 
+dato = 'Entrar'}
+ */
+res.render('login.html', {ref:ref,dato:dato})
+});    
+
+app.get('/login/:id', function(req, res) { 
+    var ref = `/producto/${req.params.id}` 
+    var dato = 'Entrar' 
+    res.render('login.html', {ref:ref,dato:dato})
+    });
+
+app.get('/producto/:id', function(req, res) {
+var input = `<input type="submit" value="Ad to cart">`
+var ruta= `/login/${req.params.id}`
+    fetch('https://raw.githubusercontent.com/juliobarbagallo/sitiodeproductos/master/data/productos.json')
+    .then(response => response.json())
+    .then(productos => {
+if (req.session.logeado === true){input = `<input type="submit" value="Ad to cart">`; ruta= `/login`}
+res.render('producto.html', {ruta:ruta,input:input,productos:productos, id:req.params.id})})
+});
+
+<<<<<<< HEAD
+
+app.get('/categorias/:categoria', function(req, res) {
+    fetch('https://raw.githubusercontent.com/juliobarbagallo/sitiodeproductos/master/data/productos.json')
+    .then(response => response.json())
+    .then(productos => res.render('categorias.html', {productos:productos, categoria:req.params.categoria}))
+});
+
+=======
 app.get('/buscar', function(req, res){
     // Obtengo los names del formulario de búsqueda con req.query
     res.send(`
@@ -34,6 +89,7 @@ app.get('/buscar', function(req, res){
     )
 
 })
+>>>>>>> 4addfca488cd7f236b92978a5a2c0195675be3cf
 
 
 app.listen(8080);
--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,39 @@
 console.log("Cargado desde el lado cliente")
+
+fetch('https://raw.githubusercontent.com/juliobarbagallo/sitiodeproductos/master/data/productos.json')
+  .then(response => response.json())
+  .then(data =>{ 
+    
+    console.log(data)
+
+  for (i in data.sort()){
+    if (i > 0){  
+    if (data[i].categoria != data[i-1].categoria){
+        document.getElementById('drop').insertAdjacentHTML(`beforeend`, `<a class="dropdown-item" href="/categorias/${data[i].categoria}">${data[i].categoria}</a>`)
+        console.log(data[i].categoria)
+    }}
+    else{
+        document.getElementById('drop').insertAdjacentHTML(`beforeend`, `<a class="dropdown-item" href="/categorias/${data[i].categoria}">${data[i].categoria}</a>`)
+        console.log(data[i].categoria)}
+    }   
+});
+
+function search(event){
+var a = event.target.value
+    console.log(a)
+var buscar = a.length
+console.log(buscar)
+fetch('https://raw.githubusercontent.com/juliobarbagallo/sitiodeproductos/master/data/productos.json')
+.then(response => response.json())
+.then(data =>{ 
+var lista = []
+for (prod of data){
+if(buscar > 0 && prod.nombre.slice(0,buscar).toLowerCase() === a.toLowerCase()){
+console.log(prod.nombre+prod.id)
+lista += `<li><a style="margin: 1% 0;" class="dropdown-item" href="/producto/${prod.id}">${prod.nombre}</a></li>`
+}
+}
+document.getElementById('buscar').innerHTML = lista  
+})
+}
+
